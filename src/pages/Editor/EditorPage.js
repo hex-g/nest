408f91b33327c9b156e3428f7a6d9e33f3cb63e7
CaioMassayuki <japa.zito@hotmail.com>
2019-05-21 21:26:26 -0300

    Add all EditorJs Dependencies

--- src/pages/Editor/EditorPage.js
+++ src/pages/Editor/EditorPage.js
@@ -1,14 +1,92 @@
 import React from 'react'
+import EditorJs from '@editorjs/editorjs'
+import Header from '@editorjs/header'
+import List from '@editorjs/list'
+import Checklist from '@editorjs/checklist'
+import Embed from '@editorjs/embed'
+import Link from '@editorjs/link'
+import SimpleImage from '@editorjs/simple-image'
+import Quote from '@editorjs/quote'
+import Code from '@editorjs/code'
+import InlineCode from '@editorjs/inline-code'
+import Table from '@editorjs/table'
+import Delimiter from '@editorjs/delimiter'
+import Personality from 'codex.editor.personality'
+import Marker from '@editorjs/marker'
+import Warning from '@editorjs/warning'
 import {
-
+  Wrapper,
 } from './EditorPage.style'
 
-const EditorPage = () => {
+const editor = new EditorJs({
+  holderId: 'editorjs',
+  tools: {
+    header: {
+      class: Header,
+      shortcut: 'CMD+SHIFT+H',
+    },
+    list: {
+      class: List,
+      inlineToolbar: true,
+      shortcut: 'CMD+SHIFT+L',
+    },
+    checklist: {
+      class: Checklist,
+      inlineToolbar: true,
+      shortcut: 'CMD+SHIFT+K',
+    },
+    embed: {
+      class: Embed,
+      config: {
+        services: {
+          youtube: true,
+          imgur: true,
+          codepen: true,
+          'twitch-video': true,
+        },
+      },
+    },
+    image: SimpleImage,
+    quote: {
+      class: Quote,
+      inlineToolbar: true,
+      shortcut: 'CMD+SHIFT+O',
+      config: {
+        quotePlaceholder: 'Enter a quote',
+        captionPlaceholder: 'Quote\'s author',
+      },
+    },
+    inlineCode: {
+      class: InlineCode,
+    },
+    code: Code,
+    table: {
+      class: Table,
+      inlineToolbar: true,
+    },
+    delimiter: Delimiter,
+    marker: {
+      class: Marker,
+      shortcut: 'CMD+SHIFT+M',
+    },
+    warning: {
+      class: Warning,
+      inlineToolbar: true,
+      shortcut: 'CMD+SHIFT+W',
+      config: {
+        titlePlaceholder: 'Title',
+        messagePlaceholder: 'Message',
+      },
+    }
+  },
+})
 
-
-return (
-
-)
+const Editor = () => {
+  return (
+    <Wrapper>
+      <div style={{ background: 'rgb(64,64,64)', color: 'white' }} id="editorjs" />
+    </Wrapper>
+  )
 }
 
-export default EditorPage
+export default Editor
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import Nest from '../pages/Nest'
+import Editor from '../pages/Editor'
 
 const Routes = () => (
   <div>
     <Route exact path="/" component={Nest} />
+    <Route path="/editor" component={Editor} />
   </div>
 )
 
