04de0f7e6ed893b0ce055857e1deb8231704d6ef
CaioMassayuki <japa.zito@hotmail.com>
2019-05-23 15:29:58 -0300

    Editor with scroll

--- src/pages/Editor/EditorPage.js
+++ src/pages/Editor/EditorPage.js
@@ -4,16 +4,16 @@ import Header from '@editorjs/header'
 import List from '@editorjs/list'
 import Checklist from '@editorjs/checklist'
 import Embed from '@editorjs/embed'
-import Link from '@editorjs/link'
 import SimpleImage from '@editorjs/simple-image'
 import Quote from '@editorjs/quote'
 import Code from '@editorjs/code'
 import InlineCode from '@editorjs/inline-code'
 import Table from '@editorjs/table'
 import Delimiter from '@editorjs/delimiter'
-import Personality from 'codex.editor.personality'
 import Marker from '@editorjs/marker'
 import Warning from '@editorjs/warning'
+import Link from '@editorjs/link'
+import Personality from 'codex.editor.personality'
 import {
   Wrapper,
 } from './EditorPage.style'
@@ -81,12 +81,21 @@ const editor = new EditorJs({
   },
 })
 
-const Editor = () => {
+const style = {
+  border: '1px solid red',
+  height: 'calc(100vh - 50px)',
+  overflowY: 'auto',
+  width: '680px',
+}
+
+const EditorPage = () => {
   return (
     <Wrapper>
-      <div style={{ background: 'rgb(64,64,64)', color: 'white' }} id="editorjs" />
+      <div
+        style={style}
+        id="editorjs" />
     </Wrapper>
   )
 }
 
-export default Editor
+export default EditorPage
