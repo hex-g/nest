17997b9967056e6a2758205d31bf63b905cb92a4
CaioMassayuki <japa.zito@hotmail.com>
2019-05-23 21:54:23 -0300

    Testing Request

--- src/pages/Editor/EditorPage.js
+++ src/pages/Editor/EditorPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import EditorJs from '@editorjs/editorjs'
 import Header from '@editorjs/header'
 import List from '@editorjs/list'
@@ -13,99 +13,123 @@ import Delimiter from '@editorjs/delimiter'
 import Marker from '@editorjs/marker'
 import Warning from '@editorjs/warning'
 import Link from '@editorjs/link'
-import Personality from 'codex.editor.personality'
+import { saveEditorTest, getUserNotes } from './EditorPage.service'
 import {
   Wrapper,
   Editor,
   SendButton,
 } from './EditorPage.style'
 
-const editor = new EditorJs({
-  holderId: 'editorjs',
-  tools: {
-    header: {
-      class: Header,
-      shortcut: 'CMD+SHIFT+H',
-    },
-    list: {
-      class: List,
-      inlineToolbar: true,
-      shortcut: 'CMD+SHIFT+L',
-    },
-    checklist: {
-      class: Checklist,
-      inlineToolbar: true,
-      shortcut: 'CMD+SHIFT+K',
-    },
-    embed: {
-      class: Embed,
-      config: {
-        services: {
-          youtube: true,
-          imgur: true,
-          codepen: true,
-          'twitch-video': true,
-        },
-      },
-    },
-    image: SimpleImage,
-    quote: {
-      class: Quote,
-      inlineToolbar: true,
-      shortcut: 'CMD+SHIFT+O',
-      config: {
-        quotePlaceholder: 'Enter a quote',
-        captionPlaceholder: 'Quote\'s author',
-      },
-    },
-    inlineCode: {
-      class: InlineCode,
-    },
-    code: Code,
-    table: {
-      class: Table,
-      inlineToolbar: true,
-    },
-    delimiter: Delimiter,
-    marker: {
-      class: Marker,
-      shortcut: 'CMD+SHIFT+M',
-    },
-    warning: {
-      class: Warning,
-      inlineToolbar: true,
-      shortcut: 'CMD+SHIFT+W',
-      config: {
-        titlePlaceholder: 'Title',
-        messagePlaceholder: 'Message',
-      },
-    }
+const EDITOR_CONFIG = {
+  header: {
+    class: Header,
+    shortcut: 'CMD+SHIFT+H',
   },
-})
+  list: {
+    class: List,
+    inlineToolbar: true,
+    shortcut: 'CMD+SHIFT+L',
+  },
+  checklist: {
+    class: Checklist,
+    inlineToolbar: true,
+    shortcut: 'CMD+SHIFT+K',
+  },
+  embed: {
+    class: Embed,
+    config: {
+      services: {
+        youtube: true,
+        imgur: true,
+        codepen: true,
+        'twitch-video': true,
+      },
+    },
+  },
+  image: SimpleImage,
+  quote: {
+    class: Quote,
+    inlineToolbar: true,
+    shortcut: 'CMD+SHIFT+O',
+    config: {
+      quotePlaceholder: 'Enter a quote',
+      captionPlaceholder: 'Quote\'s author',
+    },
+  },
+  inlineCode: {
+    class: InlineCode,
+  },
+  code: Code,
+  table: {
+    class: Table,
+    inlineToolbar: true,
+  },
+  delimiter: Delimiter,
+  marker: {
+    class: Marker,
+    shortcut: 'CMD+SHIFT+M',
+  },
+  warning: {
+    class: Warning,
+    inlineToolbar: true,
+    shortcut: 'CMD+SHIFT+W',
+    config: {
+      titlePlaceholder: 'Title',
+      messagePlaceholder: 'Message',
+    },
+  }
+}
+
+let editor
+
+const handleSendNotes = () => {
+  editor.save().then((outputData) => {
+    saveEditorTest(JSON.stringify(outputData))
+  }).catch((error) => {
+    console.log('Saving failed: ', error)
+  });
+
+}
 
 const EditorPage = () => {
 
-  React.useEffect(() => {
+  const [note, setNote] = React.useState(null)
+
+  const getNotes = async () => {
+    const response = await getUserNotes()
+    response && response.data.content && localStorage.setItem('user-note', response.data.content)
+  }
+
+  getNotes()
+
+  useEffect(() => {
+
+    let note = JSON.parse(localStorage.getItem('user-note'))
+    editor = new EditorJs({
+      autofocus: true,
+      tools: EDITOR_CONFIG,
+      data: note,
+    })
+
     setTimeout(() => {
       let codex_redactor = document.querySelector('.codex-editor__redactor')
-      codex_redactor.style.paddingBottom = '0px'
+      codex_redactor.style.paddingBottom = '0px';;
 
     }, 100)
 
-    setInterval(() => {
+    /*setInterval(() => {
       editor.save().then((outputData) => {
-        console.log('Article data: ', outputData)
+        localStorage.setItem('user-note', JSON.stringify(outputData))
       }).catch((error) => {
         console.log('Saving failed: ', error)
       });
-      console.log('Note saved')
-    }, 10000)
+    }, 15000)*/
 
   }, [])
 
   return (
     <Wrapper>
-      <SendButton>
+      <SendButton onClick={handleSendNotes}>
         Send Button
       </SendButton>
       <Editor
--- src/pages/Editor/EditorPage.service.js
+++ src/pages/Editor/EditorPage.service.js
@@ -1,17 +1,26 @@
 import { request } from '../../config/http-request'
 
-const API_DO_HIRUMITSU = ''
-const TEST_TOKEN = ''
+const API_DO_HIRUMITSU = 'http://10.3.0.70:8762'
+const TEST_TOKEN = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGhvcml0aWVzIjpbIlJPTEVfMCJdLCJpYXQiOjE1NTg2NTcxNDgsImV4cCI6MTU1ODcwMDM0OH0.t6Q3Vt01pwpMLtXC5Zyjl_rZWmYxshd3uhqXJKCjBJ6eIFWr6E0pKe0-2KPluP6AGWCCv4m27n7Pr30DJTGFIw'
+const DIRECTORY = 'Test'
 
-export const saveEditorTest = async (directory, note, token) => {
+export const saveEditorTest = async (note) => {
   return await request({
     url: `${API_DO_HIRUMITSU}/kirby/note`,
     method: 'post',
-    headers: { Authorization: token},
+    headers: { Authorization: TEST_TOKEN},
     data: {
-      path: directory,
+      path: DIRECTORY,
       content: note,
     }
 
+  })
+}
+
+export const getUserNotes = async () => {
+  return await request({
+    url: `${API_DO_HIRUMITSU}/kirby/note?path=${DIRECTORY}`,
+    method: 'get',
+    headers: { Authorization: TEST_TOKEN},
   })
 }
