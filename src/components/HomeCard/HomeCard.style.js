ab74c2b5ca78166c2dc6948fbab241e55748a545
Luizca95 <Luizca95@hotmail.com>
2019-06-17 20:02:07 -0300

    Adjust style and add news tab

--- src/components/HomeCard/HomeCard.style.js
+++ src/components/HomeCard/HomeCard.style.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
-import { cardBackground } from '../Layout'
+import { design, cardBackground } from '../Layout'
 
 export const Wrapper = styled.div`
+  height: 115px;
   padding: 16px 16px;
   margin: 8px 0;
-  display: flex;
-  justify-content: space-around;
+  display: grid;
+  grid-template-columns: 1fr 10fr 1fr;
   background-color: ${cardBackground};
   border-radius: 10px;
   box-shadow: 0 2px 5px rgba(21,21,21,0.2);
@@ -15,12 +16,13 @@ export const UserPicture = styled.img`
   max-width: 80px;
   max-height: 80px;
   object-fit: cover;
+  align-self: center;
 `
 
 export const Content = styled.div`
   margin-left: 30px;
-  font-size: 18px;  
-  flex-grow: 1;
+  font-size: 18px;
+  align-self: center;
 `
 
 export const Username = styled.p`
@@ -32,5 +34,30 @@ export const Description = styled.p`
 `
 
 export const Date = styled.p`
+  text-align: right;
   font-size: 12px;
+`
+
+export const Tag = styled.button`
+  background: ${props => {
+    if (props.color <= 1) {
+      return design.heliotrope
+    } else if (props.color >= 2 && props.color <= 3) {
+      return design.carnation
+    } else if (props.color >= 4 && props.color <= 5) {
+      return design.eletricViolet
+    } else if (props.color >= 6 && props.color <= 7) {
+      return design.laPalma
+    } else if (props.color >= 8 && props.color <= 9) {
+      return design.catalinaBlue
+    } else {
+      return design.gold
+    }
+  }};
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  cursor: pointer;
+  color: ${design.wildSand};
+  margin: 8px 8px 0 0;
 `
--- src/pages/Nest/Nest.js
+++ src/pages/Nest/Nest.js
@@ -11,20 +11,39 @@ import {
   Content
 } from './Nest.style'
 
-const EXAMPLE_HORIZONTAL_NAVBAR = ['AVISOS']
+const EXAMPLE_HORIZONTAL_NAVBAR = ['STATUS', 'AVISOS']
 
 const EXAMPLE_CARD_POSTS = [
   {
     user: 'Germano',
     profilePic: 'https://media.licdn.com/dms/image/C4D03AQHv3UDt2OH7uw/profile-displayphoto-shrink_200_200/0?e=1562198400&v=beta&t=qcsJy1xt4V8brdPvHXDU6WcBSnEr2mkH-FpmcFZRonU',
-    description: 'Construir uma API REST não é tão difícil quanto parece. Neste tutorial vamos precisar das ferramentas listadas abaixo...',
+    description: 'compartilhou a anotação "REST API com Node.Js" na HiveShare',
     date: '20 Jan 2019',
+    tag: ['Backend', 'Frontend', 'Node'],
   },
   {
     user: 'Germano',
     profilePic: 'https://media.licdn.com/dms/image/C4D03AQHv3UDt2OH7uw/profile-displayphoto-shrink_200_200/0?e=1562198400&v=beta&t=qcsJy1xt4V8brdPvHXDU6WcBSnEr2mkH-FpmcFZRonU',
-    description: 'Aprenda a trabalhar com uma das ferramentas mais utilizadas no mercado de Java...',
+    description: 'precisa de ajuda com "Como passar props para keyframes no styled-components utilizando React?" na HiveCentral',
     date: '20 Jan 1999',
+    tag: ['Frontend', 'CSS'],
+  },
+]
+
+const NEWS = [
+  {
+    user: 'Thiago Bonaceli',
+    profilePic: 'https://media.licdn.com/dms/image/C4D03AQHv3UDt2OH7uw/profile-displayphoto-shrink_200_200/0?e=1562198400&v=beta&t=qcsJy1xt4V8brdPvHXDU6WcBSnEr2mkH-FpmcFZRonU',
+    description: 'PyLadies São Paulo',
+    date: '20 Jan 2019',
+    tag: [],
+  },
+  {
+    user: 'Célia Taniwaki',
+    profilePic: 'https://media.licdn.com/dms/image/C4D03AQHv3UDt2OH7uw/profile-displayphoto-shrink_200_200/0?e=1562198400&v=beta&t=qcsJy1xt4V8brdPvHXDU6WcBSnEr2mkH-FpmcFZRonU',
+    description: 'Avaliação continuada 03',
+    date: '20 Jan 1999',
+    tag: [],
   },
 ]
 
@@ -34,23 +53,35 @@ const Nest = ({
   options = EXAMPLE_HORIZONTAL_NAVBAR
 }) => {
 
+  const [page, setPage] = React.useState(posts)
+
+  const switchPage = () => {
+    if (page === EXAMPLE_CARD_POSTS) {
+      setPage(NEWS)
+      return
+    }
+    setPage(EXAMPLE_CARD_POSTS)
+  }
+
   return (
     <Page>
       <Container>
         <Form>
           <SearchBox />
           <HorizontalNavigation>
-            <HorizontalNavbar options={options} />
+            <HorizontalNavbar options={options} listOption={switchPage} />
           </HorizontalNavigation>
         </Form>
         <Content>
-          {posts.map((post, index) => (
+          {page.map((post, index) => (
             <HomeCard
-            key={`${post.user}-${index}`}
-            userPicture={post.profilePic}
-            username={post.user}
-            description={post.description}
-            postDate={post.date}
+              key={`${post.user}-${index}`}
+              userPicture={post.profilePic}
+              username={post.user}
+              description={post.description}
+              postDate={post.date}
+              tag={post.tag}
+              index = {index}
             />
           ))}
         </Content>
