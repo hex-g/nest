277b1d862ca526cbf871935fac7214937d2ae843
eiling <vmhirumitsu@gmail.com>
2019-06-18 19:41:19 -0300

    Admin page

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,20 +1,77 @@
 import React, { useState } from 'react'
 import {
   AdminWrapper,
-  AdminTitle,
-  AdminInput
+  Title,
+  FormTitle,
+  Choice,
+  Form,
+  FormElement
 } from './Admin.style'
 
 const Admin = () => {
-  const [text, setText] = useState('default')
+  const [currentForm, setCurrentForm] = useState('nada')
 
   return (
     <AdminWrapper>
-      <AdminTitle>Admin</AdminTitle>
-      <label for="admin-combo">{text}</label>
-      <AdminInput id="admin-combo" type="select" onKeyUp={() => (setText('fff'))} />
+      <Title>Deseja cadastrar:</Title>
+      <Choice>
+        <input id="student-radio" type="radio" name="choice" onClick={() => setCurrentForm('student')} />
+        <label for="student-radio">Aluno</label>
+        <input id="pedagogue-radio" type="radio" name="choice" onClick={() => setCurrentForm('pedagogue')} />
+        <label for="pedagogue-radio">Professor</label>
+      </Choice>
+      <FormTitle show={currentForm === 'student'}>Cadastro do aluno:</FormTitle>
+      <Form show={currentForm === 'student'}>
+          <FormElement>
+            <label for="name-student">Nome</label>
+            <input id="name-student" />
+          </FormElement>
+          <FormElement>
+            <label for="username-student">Usuário</label>
+            <input id="username-student" />
+          </FormElement>
+          <FormElement>
+            <label for="ra-student">RA</label>
+            <input id="ra-student" />
+          </FormElement>
+          <FormElement>
+            <label for="lastname-student">Sobrenome</label>
+            <input id="lastname-student" />
+          </FormElement>
+          <FormElement>
+            <label for="password-student">Senha</label>
+            <input id="password-student" />
+          </FormElement>
+      </Form>
+      <FormTitle show={currentForm === 'pedagogue'}>Cadastro do professor:</FormTitle>
+      <Form show={currentForm === 'pedagogue'}>
+          <FormElement>
+            <label for="name-pedagogue">Nome</label>
+            <input id="name-pedagogue" />
+          </FormElement>
+          <FormElement>
+            <label for="username-pedagogue">Usuário</label>
+            <input id="username-pedagogue" />
+          </FormElement>
+          <FormElement>
+            <label for="rm-pedagogue">RM</label>
+            <input id="rm-pedagogue" />
+          </FormElement>
+          <FormElement>
+            <label for="lastname-pedagogue">Sobrenome</label>
+            <input id="lastname-pedagogue" />
+          </FormElement>
+          <FormElement>
+            <label for="password-pedagogue">Senha</label>
+            <input id="password-pedagogue" />
+          </FormElement>
+      </Form>
+      <button type='button' onClick={(e) => {
+        console.log(e)
+      }}>Finalizar</button>
     </AdminWrapper>
   )
 }
+
 
 export default Admin
--- src/pages/Admin/Admin.style.js
+++ src/pages/Admin/Admin.style.js
@@ -1,22 +1,97 @@
 import styled from 'styled-components'
-import {design, textColor} from '../../components/Layout'
+import { design, textColor } from '../../components/Layout'
 
 export const AdminWrapper = styled.section`
+  height: 100%;
   display: flex;
   flex-direction: column;
   padding: 0 25%;
   justify-content: center;
+  font-family: ${design.secondaryFont};
+  button {
+    margin: 32px 0;
+    width: 140px;
+    height: 50px;
+    border: none;
+    border-radius: 7px;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+    background-color: ${design.burntSienna};
+    font-size: 17px;
+    font-weight: bold;
+    font-style: normal;
+    letter-spacing: normal;
+    text-align: center;
+    color: ${design.wildSand};
+  }
 `
 
-export const AdminTitle = styled.h2`
-color: ${design.burntSienna};
+export const Title = styled.h2`
+  margin: 24px 0;
+  font-size: 32px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: ${design.burntSienna};
 `
 
-export const AdminInput = styled.input`
-  font-size: ${design.mediumText};
-  color: ${textColor};
+export const FormTitle = styled.h2`
+  display: ${props => props.show ? 'block' : 'none'};
+  margin: 24px 0;
+  font-size: 32px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: ${design.burntSienna};
+`
 
-  &::placeholder {
-    font-size: ${design.mediumText};
+export const Choice = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  input {
+    width: 30px;
+    height: 30px;
+    border: solid 2px #979797;
+    margin: 0 10px 0 0;
+  }
+  label {
+    margin-right: 20px;
+    font-size: 18px;
+    font-weight: normal;
+    font-style: normal;
+    letter-spacing: normal;
+    color: ${design.outerSpace};
+  }
+`
+
+export const Form = styled.form`
+  display: ${props => props.show ? 'grid' : 'none'};
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+`
+
+export const FormElement = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+  label {
+    margin-bottom: 12px;
+    font-size: 18px;
+    font-weight: bold;
+    font-style: normal;
+    letter-spacing: normal;
+    color: ${design.burntSienna};
+  }
+  input {
+    width: 308px;
+    height: 34px;
+    border: none;
+    border-radius: 8px;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.5);
+    background-color: ${design.wildSand};
   }
 `
