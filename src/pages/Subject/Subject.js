9dba2fbbbb07afe3b0e80819ae6461a666e4739d
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-06-18 19:08:15 -0300

    Testes

--- src/pages/Subject/Subject.js
+++ src/pages/Subject/Subject.js
@@ -3,7 +3,6 @@ import React from 'react'
 export const Subject =  () => {
     const urlParams = new URLSearchParams(window.location.search);
     const subject = urlParams.get('subject');
-    
 
     return(
         <div>{subject}</div>
